d1e0e1c13d2d9715459ff242619b8f80c35a961b
test_git <geranium236@gmail.com>
2020-03-29 14:01:13 +0900

    game4 숫자 이외의 값 입력하면 발생하는 문제 해결

--- TeamProject_REAL/src/Game4.java
+++ TeamProject_REAL/src/Game4.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Game4 extends Game{
 	int x;// 랜덤으로 생성할 숫자
 	int y;
-	int z;// 사람이 적을 답
+	int z;
+	String input;// 사람이 적을 답
 	int answer;// 계산된 답
 
 //##
@@ -13,7 +14,7 @@ public class Game4 extends Game{
 	Scanner scan;
 	Random random;
 	Game4() {
-//		score4 = 0;	
+		input = "";
 		newScore = 0; 
 		scan = new Scanner(System.in);
 		random = new Random();	
@@ -25,13 +26,13 @@ public class Game4 extends Game{
 		score4 = MGSystem.currentUser.getGame4Score();
 		System.out.println();
 		System.out.println("**************************************************");
-		System.out.println("                         구구단 게임");
+		System.out.println("                     구구단 게임");
 		System.out.println("**************************************************");
 		System.out.println();
-		System.out.println("		답을 빨리 맞출 수록 더 많은 점수를 획득할 수 있습니다 ");
+		System.out.println("	답을 빨리 맞출 수록 더 많은 점수를 획득할 수 있습니다 ");
 		System.out.println();
 		System.out.println("**************************************************");
-		System.out.println("		1. 게임 실행하기  	 0. 이전 메뉴로 돌아가기");
+		System.out.println("	1. 게임 실행하기  	 0. 이전 메뉴로 돌아가기");
 		System.out.println("**************************************************");
 		int menu = 0;
 		do {
@@ -50,63 +51,56 @@ public class Game4 extends Game{
 	}
 	
 	public void guguMain() {
-		System.out.println("gugumain before while ");
 
 		toGdp : while(true) {
-			System.out.println("gugumain while start");
 
 			switch(this.intro()) {
-			case 1 : System.out.println("gugumain switch case1 before makeSubGame");
-				this.makeSubGame();
-				System.out.println("gugumain switch case1 after makeSubGame");
+			case 1 : this.makeSubGame();
 				break;
-			case 0 : System.out.println("이전메뉴로 돌아갑니다.");
+			case 0 : 
 				break toGdp;
 			}
-			System.out.println("gugumain switch end");
-
 		}
-	System.out.println("gugumain while end");
-
 	}
 
 	
 	void makeSubGame() {
-		int i = 0;// 문제 반복 // i를 로컬변수로 설정해야 메소드가 실행될 때마다 만들어지겠죠?
-		System.out.println("makeSubGame start");
+		int i = 0;
+        start = System.currentTimeMillis();
 
-            start = System.currentTimeMillis();
-    		System.out.println("makeSubGame before while");
-
-		while (i < 5) {	// $$ i를 인스턴스 변수로 뽑으면 게임이 한번밖에 실행이 안됩니다 ^^
-    		System.out.println("makeSubGame while start");
-
+		while (i < 5) {	
 			i++;
 			x = random.nextInt(9) + 1;
 			y = random.nextInt(9) + 1;
 			answer = x * y;
 			System.out.println(x + "X" + y + "=?");
-			
-    		System.out.println("makeSubGame while before scanner");
 
-			z = Integer.parseInt(scan.nextLine());
-//			z = scan.nextInt();
-    		System.out.println("makeSubGame while after scanner");
+    		
+    		while(true) {
+    			try {
+    				input = scan.nextLine();
+    				if(isNumeric(input)) {
+    					z = Integer.parseInt(input);
+    					break;
+    				}else {
+    					throw new Exception("숫자만 입력하세요");
+  
+    				}
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+    		}
 
-    		System.out.println("makeSubGame while before if");
-			if (z == answer) {
+    		if (z == answer) {
 				System.out.println("정답입니다!");
 				newScore += 15;
 			} else {
 				System.out.println("오답입니다!");
 			}
-    		System.out.println("makeSubGame while after if");
-
 		}
 		end = System.currentTimeMillis();
 		float playTime = (end - start)/1000f;
-//		System.out.println("원래 점수 newScore : "  + newScore); #삭제가능
-		int bonus = (int)playTime; // 시간가산점
+		int bonus = (int)playTime; // 시간가산점용
 		if(playTime < 6) {
 			bonus = 30;
 		}
@@ -120,19 +114,24 @@ public class Game4 extends Game{
 			System.out.println("최종점수는 100점 만점 중" + newScore + " 점입니다." + "\n아쉽게 만점을 받지 못했습니다\n" + "분발하세요!");
 		}
 		
-		System.out.println("makeSubGame while before autoSave");
 		scoreAutoSave(newScore);
-		System.out.println("makeSubGame while after autoSave");
-
 	}
+	
 	@Override
 	public void scoreAutoSave(int newScore) {
 		if(newScore > score4) {
 			score4 = newScore;
 			MGSystem.currentUser.setGame4Score(score4); // 커런트유저 점수 세팅
-			System.out.println("before CU totalsc : "+MGSystem.currentUser.getTotalScore());//확인용
 			MGSystem.currentUser.setTotalScore(score1 + score2 + score3 + score4);
-			System.out.println("After CU totalsc : "+MGSystem.currentUser.getTotalScore());//확인용
 		}
 	}
+	
+	public boolean isNumeric(String input) { 
+		try { Double.parseDouble(input); 
+				return true; 
+			} catch (NumberFormatException e) { 
+				return false; 
+			} 
+		}
+
 }
